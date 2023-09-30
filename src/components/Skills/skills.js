f9cb9d32000490241201fa65c6a1ce857c83dab7
BenAwuor2 <99531759+BenAwuor2@users.noreply.github.com>
2023-09-30 10:50:25 +0300

    add-ons

--- src/components/Skills/skills.js
+++ src/components/Skills/skills.js
@@ -7,7 +7,7 @@ import react from "../../assets/React_logo.png";
 const Skills = () => {
     return (
         <section id="skills">
-            <span className="skillTittle"> What I do</span>
+            <h1> What I do</h1>
             <span className="skillDesc"> I turn ideas into reality by leveraging this technologies listed below to develop fully responsive and interractive websites</span>
             <div className="skillBars">
                 <div className="skillBar">
